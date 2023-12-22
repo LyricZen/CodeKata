5188a91947971d7ae3ff3d6fb488a65feaa8654e
LyricZen <kohoon0129@gmail.com>
2023-12-22 09:49:29 +0900

    Initial commit

---
+++ src/kata/Solution1.java
@@ -0,0 +1,12 @@
+package kata;
+
+public class Solution1 {
+    public int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
